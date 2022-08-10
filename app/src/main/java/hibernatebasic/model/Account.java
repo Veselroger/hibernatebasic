e31c02bfca8804fdbc6777de7cb999b5d48042a6
Viacheslav <slavastap@gmail.com>
2022-08-10 12:42:30 +0200

    minor fix

--- app/src/main/java/hibernatebasic/model/Account.java
+++ app/src/main/java/hibernatebasic/model/Account.java
@@ -1,14 +1,16 @@
 package hibernatebasic.model;
 
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Data // Добавит геттер + сеттер
-@NoArgsConstructor // Для JPA
 @Entity
+@NoArgsConstructor // Для JPA
+@Setter @Getter
 public class Account {
     @Id
     private Long id;
